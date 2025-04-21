c8377a44d7b19434fc5b8a8a09797d76d26c1165
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-04-21 18:12:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
+API_ID = os.getenv("API_ID", "27823209")
+API_HASH = os.getenv("API_HASH", "1d693fcf3bfea119ca1d9057b08a4495")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-ADMIN = int(os.getenv("ADMIN", ""))
+ADMIN = int(os.getenv("ADMIN", "6004928770"))
 
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001738045764"))
 
-DB_URI = os.getenv("DB_URI", "")
-DB_NAME = os.getenv("DB_NAME", "")
+DB_URI = os.getenv("DB_URI", "mongodb+srv://aibotmv28:aibotmv28@cluster0.mkzpuv8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.getenv("DB_NAME", "Cluster0")
 
-IS_FSUB = bool(os.environ.get("FSUB", False)) # Set "True" For Force Subscribe Enable
-AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "") # Add Multiple Channels iD By Space
+IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Force Subscribe Enable
+AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-1002327045567") # Add Multiple Channels iD By Space
 AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")] # DONT TOUCH
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyC7F6hGS9ZNMdUDXueQfhnhTZsYIWG9IXk")
