f54556b73a5e21fa78b6c3563b6b2d3bbb4b77b1
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-04-27 04:37:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ DB_URI = os.getenv("DB_URI", "mongodb+srv://aibotmv28:aibotmv28@cluster0.mkzpuv8
 DB_NAME = os.getenv("DB_NAME", "Cluster0")
 
 IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Force Subscribe Enable
-AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-1002327045567") # Add Multiple Channels iD By Space
+AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-1002327045567 -1002414808225") # Add Multiple Channels iD By Space
 AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")] # DONT TOUCH
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyDojXqjZT7VvCVIQ2-l3mkmopbMoyo0W7Q")
